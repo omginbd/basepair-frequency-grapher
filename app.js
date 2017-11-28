44db6be87bee66ff16af9ea0da5937bcf2234185
Michael Collier <mikec@kuali.co>
2017-11-28 11:31:38 -0700

    Basic skeleton

---
+++ app.js
@@ -0,0 +1,35 @@
+/*******************
+ *
+ * file: stringified version of uploaded file
+ *
+ * returns json representation of reads:
+ * {
+ *  "1-mers": [
+ *    {
+ *      "count": 12345,
+ *      "proportion": 0.75
+ *    },
+ *    ...
+ *  ],
+ *  "2-mers": [...],
+ *  ...
+ * }
+ *
+ *******************/
+const parseFile = file => {}
+
+/*******************
+ *
+ * read1: json representation of file1
+ * read2: json representation of file2
+ * (see above for data format)
+ *
+ * returns multidimensional array of p-values:
+ * [
+ *  [.1, .2, .3, ...],
+ *  [...],
+ *  ...
+ * ]
+ *
+ *******************/
+const compareReads = (read1, read2) => {}
