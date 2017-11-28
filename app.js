0e87572dd675a68d6a407c97229f7961b5b27124
Bo Pace <bopace@gmail.com>
2017-11-28 15:29:47 -0700

    adding logic to parse a file
    parses a file into a js object

--- app.js
+++ app.js
@@ -4,19 +4,122 @@
  *
  * returns json representation of reads:
  * {
- *  "1-mers": [
+ *  "onemers": [
  *    {
- *      "count": 12345,
- *      "proportion": 0.75
+ *      "A":
+ *        {
+ *          "count": 12345,
+ *          "proportion": 0.75
+ *        },
+ *      ...
  *    },
  *    ...
  *  ],
- *  "2-mers": [...],
+ *  "twomers": [...],
  *  ...
  * }
  *
  *******************/
-const parseFile = file => {}
+const parseFile = file => {
+  file = file.replace(/(Position: [0-9]*)|(GC content: [0-9]*.[0-9]*)/g, "");
+  var lines = file.split("\n");
+  lines = lines.map((line) => line.trim());
+  lines = lines.map((line) => line.split(';'));
+
+  var parsedObj = new Object();
+  parsedObj.onemers   = new Array();
+  parsedObj.twomers   = new Array();
+  parsedObj.threemers = new Array();
+  parsedObj.fourmers  = new Array();
+
+  var unparsedOnemers   = new Array();
+  var unparsedTwomers   = new Array();
+  var unparsedThreemers = new Array();
+  var unparsedFourmers  = new Array();
+
+  lines.forEach(function(line) {
+    var currLine = line[0];
+    if (currLine.startsWith("A=")) {
+      unparsedOnemers.push(line);
+    } else if (currLine.startsWith("AA=")) {
+      unparsedTwomers.push(line);
+    } else if (currLine.startsWith("AAA=")) {
+      unparsedThreemers.push(line);
+    } else if (currLine.startsWith("AAAA=")) {
+      unparsedFourmers.push(line);
+    }
+  });
+
+  // Parse onemers
+  unparsedOnemers.forEach(function(onemer) {
+    // make a onemer object
+    var newOnemer = new Object();
+    // for every grouping in onemer
+    onemer.forEach(function(grouping, index, arr) {
+      var parsedGrouping = grouping.replace(/(=\[)|(:)/g, ";").replace(/(\])/g, "").split(";");
+      newOnemer[parsedGrouping[0]] = new Object();
+      newOnemer[parsedGrouping[0]] = {
+        count: Number(parsedGrouping[1]),
+        proportion: Number(parsedGrouping[2])
+      };
+    });
+    // put the new onemer in the parsed object
+    parsedObj.onemers.push(newOnemer);
+  });
+
+  // Parse twomers
+  unparsedTwomers.forEach(function(twomer) {
+    // make a twomer object
+    var newTwomer = new Object();
+    // for every grouping in twomer
+    twomer.forEach(function(grouping, index, arr) {
+      var parsedGrouping = grouping.replace(/(=\[)|(:)/g, ";").replace(/(\])/g, "").split(";");
+      newTwomer[parsedGrouping[0]] = new Object();
+      newTwomer[parsedGrouping[0]] = {
+        count: Number(parsedGrouping[1]),
+        proportion: Number(parsedGrouping[2])
+      };
+    });
+    // put the new twomer in the parsed object
+    parsedObj.twomers.push(newTwomer);
+  });
+
+  // Parse threemers
+  unparsedThreemers.forEach(function(threemer) {
+    // make a threemer object
+    var newThreemer = new Object();
+    // for every grouping in threemer
+    threemer.forEach(function(grouping, index, arr) {
+      var parsedGrouping = grouping.replace(/(=\[)|(:)/g, ";").replace(/(\])/g, "").split(";");
+      newThreemer[parsedGrouping[0]] = new Object();
+      newThreemer[parsedGrouping[0]] = {
+        count: Number(parsedGrouping[1]),
+        proportion: Number(parsedGrouping[2])
+      };
+    });
+    // put the new threemer in the parsed object
+    parsedObj.threemers.push(newThreemer);
+  });
+
+  // Parse fourmers
+  unparsedFourmers.forEach(function(fourmer) {
+    // make a fourmer object
+    var newFourmer = new Object();
+    // for every grouping in fourmer
+    fourmer.forEach(function(grouping, index, arr) {
+      var parsedGrouping = grouping.replace(/(=\[)|(:)/g, ";").replace(/(\])/g, "").split(";");
+      newFourmer[parsedGrouping[0]] = new Object();
+      newFourmer[parsedGrouping[0]] = {
+        count: Number(parsedGrouping[1]),
+        proportion: Number(parsedGrouping[2])
+      };
+    });
+    // put the new fourmer in the parsed object
+    parsedObj.fourmers.push(newFourmer);
+  });
+
+  return parsedObj;
+}
 
 /*******************
  *
