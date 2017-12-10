89da53be7cffbf67523d901ca9520b38b563aea5
Michael Collier <mikec@kuali.co>
2017-12-10 13:58:05 -0700

    basic table structure

--- app.js
+++ app.js
@@ -21,104 +21,116 @@
  *
  *******************/
 const parseFile = file => {
-  file = file.replace(/(Position: [0-9]*)|(GC content: [0-9]*.[0-9]*)/g, "");
-  var lines = file.split("\n");
-  lines = lines.map((line) => line.trim());
-  lines = lines.map((line) => line.split(';'));
+  file = file.replace(/(Position: [0-9]*)|(GC content: [0-9]*.[0-9]*)/g, "")
+  var lines = file.split("\n")
+  lines = lines.map(line => line.trim())
+  lines = lines.map(line => line.split(";"))
 
-  var parsedObj = new Object();
-  parsedObj.onemers   = new Array();
-  parsedObj.twomers   = new Array();
-  parsedObj.threemers = new Array();
-  parsedObj.fourmers  = new Array();
+  var parsedObj = new Object()
+  parsedObj.onemers = new Array()
+  parsedObj.twomers = new Array()
+  parsedObj.threemers = new Array()
+  parsedObj.fourmers = new Array()
 
-  var unparsedOnemers   = new Array();
-  var unparsedTwomers   = new Array();
-  var unparsedThreemers = new Array();
-  var unparsedFourmers  = new Array();
+  var unparsedOnemers = new Array()
+  var unparsedTwomers = new Array()
+  var unparsedThreemers = new Array()
+  var unparsedFourmers = new Array()
 
   lines.forEach(function(line) {
-    var currLine = line[0]; 
+    var currLine = line[0]
     if (currLine.startsWith("A=")) {
-      unparsedOnemers.push(line);
+      unparsedOnemers.push(line)
     } else if (currLine.startsWith("AA=")) {
-      unparsedTwomers.push(line);
+      unparsedTwomers.push(line)
     } else if (currLine.startsWith("AAA=")) {
-      unparsedThreemers.push(line);
+      unparsedThreemers.push(line)
     } else if (currLine.startsWith("AAAA=")) {
-      unparsedFourmers.push(line);
+      unparsedFourmers.push(line)
     }
-  });
+  })
 
   // Parse onemers
   unparsedOnemers.forEach(function(onemer) {
     // make a onemer object
-    var newOnemer = new Object();
+    var newOnemer = new Object()
     // for every grouping in onemer
     onemer.forEach(function(grouping, index, arr) {
-      var parsedGrouping = grouping.replace(/(=\[)|(:)/g, ";").replace(/(\])/g, "").split(";");
-      newOnemer[parsedGrouping[0]] = new Object();
+      var parsedGrouping = grouping
+        .replace(/(=\[)|(:)/g, ";")
+        .replace(/(\])/g, "")
+        .split(";")
+      newOnemer[parsedGrouping[0]] = new Object()
       newOnemer[parsedGrouping[0]] = {
         count: Number(parsedGrouping[1]),
         proportion: Number(parsedGrouping[2])
-      };
-    });
+      }
+    })
     // put the new onemer in the parsed object
-    parsedObj.onemers.push(newOnemer);
-  });
+    parsedObj.onemers.push(newOnemer)
+  })
 
   // Parse twomers
   unparsedTwomers.forEach(function(twomer) {
     // make a twomer object
-    var newTwomer = new Object();
+    var newTwomer = new Object()
     // for every grouping in twomer
     twomer.forEach(function(grouping, index, arr) {
-      var parsedGrouping = grouping.replace(/(=\[)|(:)/g, ";").replace(/(\])/g, "").split(";");
-      newTwomer[parsedGrouping[0]] = new Object();
+      var parsedGrouping = grouping
+        .replace(/(=\[)|(:)/g, ";")
+        .replace(/(\])/g, "")
+        .split(";")
+      newTwomer[parsedGrouping[0]] = new Object()
       newTwomer[parsedGrouping[0]] = {
         count: Number(parsedGrouping[1]),
         proportion: Number(parsedGrouping[2])
-      };
-    });
+      }
+    })
     // put the new twomer in the parsed object
-    parsedObj.twomers.push(newTwomer);
-  });
+    parsedObj.twomers.push(newTwomer)
+  })
 
   // Parse threemers
   unparsedThreemers.forEach(function(threemer) {
     // make a threemer object
-    var newThreemer = new Object();
+    var newThreemer = new Object()
     // for every grouping in threemer
     threemer.forEach(function(grouping, index, arr) {
-      var parsedGrouping = grouping.replace(/(=\[)|(:)/g, ";").replace(/(\])/g, "").split(";");
-      newThreemer[parsedGrouping[0]] = new Object();
+      var parsedGrouping = grouping
+        .replace(/(=\[)|(:)/g, ";")
+        .replace(/(\])/g, "")
+        .split(";")
+      newThreemer[parsedGrouping[0]] = new Object()
       newThreemer[parsedGrouping[0]] = {
         count: Number(parsedGrouping[1]),
         proportion: Number(parsedGrouping[2])
-      };
-    });
+      }
+    })
     // put the new threemer in the parsed object
-    parsedObj.threemers.push(newThreemer);
-  });
+    parsedObj.threemers.push(newThreemer)
+  })
 
   // Parse fourmers
   unparsedFourmers.forEach(function(fourmer) {
     // make a fourmer object
-    var newFourmer = new Object();
+    var newFourmer = new Object()
     // for every grouping in fourmer
     fourmer.forEach(function(grouping, index, arr) {
-      var parsedGrouping = grouping.replace(/(=\[)|(:)/g, ";").replace(/(\])/g, "").split(";");
-      newFourmer[parsedGrouping[0]] = new Object();
+      var parsedGrouping = grouping
+        .replace(/(=\[)|(:)/g, ";")
+        .replace(/(\])/g, "")
+        .split(";")
+      newFourmer[parsedGrouping[0]] = new Object()
       newFourmer[parsedGrouping[0]] = {
         count: Number(parsedGrouping[1]),
         proportion: Number(parsedGrouping[2])
-      };
-    });
+      }
+    })
     // put the new fourmer in the parsed object
-    parsedObj.fourmers.push(newFourmer);
-  });
+    parsedObj.fourmers.push(newFourmer)
+  })
 
-  return parsedObj;
+  return parsedObj
 }
 
 /*******************
@@ -143,200 +155,198 @@ const parseFile = file => {
  *
  *******************/
 const compareReads = (parsed1, parsed2) => {
-    // z = ((p1 - p2) - 0) / (Math.sqrt(po * (1 - po) * (1/n1 + 1/n2)))
-    var pValueObjects = new Object();
-    pValueObjects.onemers   = new Array();
-    pValueObjects.twomers   = new Array();
-    pValueObjects.threemers = new Array();
-    pValueObjects.fourmers  = new Array();
-    var i
-    
-    //parse onemers
-    for (i = 0; i < parsed1.onemers.length; i++){
+  // z = ((p1 - p2) - 0) / (Math.sqrt(po * (1 - po) * (1/n1 + 1/n2)))
+  var pValueObjects = new Object()
+  pValueObjects.onemers = new Array()
+  pValueObjects.twomers = new Array()
+  pValueObjects.threemers = new Array()
+  pValueObjects.fourmers = new Array()
+  var i
 
-      var onemer1 = parsed1.onemers[i];
-      var n1 = 0;
-      Object.keys(onemer1).forEach(function(nucleotide){
-        n1 = n1 + onemer1[nucleotide].count;
-      })
+  //parse onemers
+  for (i = 0; i < parsed1.onemers.length; i++) {
+    var onemer1 = parsed1.onemers[i]
+    var n1 = 0
+    Object.keys(onemer1).forEach(function(nucleotide) {
+      n1 = n1 + onemer1[nucleotide].count
+    })
 
-      var onemer2 = parsed2.onemers[i];
-      var n2 = 0;
-      Object.keys(onemer2).forEach(function(nucleotide){
-        n2 = n2 + onemer2[nucleotide].count;
-      })
-      
-      nucleotides = new Array();
+    var onemer2 = parsed2.onemers[i]
+    var n2 = 0
+    Object.keys(onemer2).forEach(function(nucleotide) {
+      n2 = n2 + onemer2[nucleotide].count
+    })
 
-      Object.keys(onemer1).forEach(function(nucleotide){
-        var p1 =  onemer1[nucleotide].proportion
-        var p2 = onemer2[nucleotide].proportion
-        var y1 =  onemer1[nucleotide].count
-        var y2 = onemer2[nucleotide].count
-        var po = (y1+y2)/(n1+n2)
-        var z = ((p1 - p2) - 0) / (Math.sqrt(po * (1 - po) * (1/n1 + 1/n2)))
-        var pValue = GetZPercent(z)
-        nucleotides.push(pValue)      
-        console.log("ZSCORE: ", z) 
-      })
+    nucleotides = new Array()
 
-      var newOnemer = new Object();
-      newOnemer = {
-        A: nucleotides[0],
-        C: nucleotides[1],
-        G: nucleotides[2],
-        T: nucleotides[3]
-      }
-      pValueObjects.onemers.push(newOnemer)
+    Object.keys(onemer1).forEach(function(nucleotide) {
+      var p1 = onemer1[nucleotide].proportion
+      var p2 = onemer2[nucleotide].proportion
+      var y1 = onemer1[nucleotide].count
+      var y2 = onemer2[nucleotide].count
+      var po = (y1 + y2) / (n1 + n2)
+      var z = (p1 - p2 - 0) / Math.sqrt(po * (1 - po) * (1 / n1 + 1 / n2))
+      var pValue = GetZPercent(z)
+      nucleotides.push(pValue)
+      // console.log("ZSCORE: ", z)
+    })
+
+    var newOnemer = new Object()
+    newOnemer = {
+      A: nucleotides[0],
+      C: nucleotides[1],
+      G: nucleotides[2],
+      T: nucleotides[3]
     }
+    pValueObjects.onemers.push(newOnemer)
+  }
 
-    //parse twomers
-    for (i = 0; i < parsed1.twomers.length; i++){
-      
-            var twomer1 = parsed1.twomers[i];
-            var n1 = 0;
-            Object.keys(twomer1).forEach(function(nucleotide){
-              n1 = n1 + twomer1[nucleotide].count;
-            })
-      
-            var twomer2 = parsed2.twomers[i];
-            var n2 = 0;
-            Object.keys(twomer2).forEach(function(nucleotide){
-              n2 = n2 + twomer2[nucleotide].count;
-            })
+  //parse twomers
+  for (i = 0; i < parsed1.twomers.length; i++) {
+    var twomer1 = parsed1.twomers[i]
+    var n1 = 0
+    Object.keys(twomer1).forEach(function(nucleotide) {
+      n1 = n1 + twomer1[nucleotide].count
+    })
 
-            nucleotides = new Array();
+    var twomer2 = parsed2.twomers[i]
+    var n2 = 0
+    Object.keys(twomer2).forEach(function(nucleotide) {
+      n2 = n2 + twomer2[nucleotide].count
+    })
 
-            Object.keys(twomer1).forEach(function(nucleotide){
-              var p1 =  twomer1[nucleotide].proportion
-              var p2 = twomer2[nucleotide].proportion
-              var y1 =  twomer1[nucleotide].count
-              var y2 = twomer2[nucleotide].count
-              var po = (y1+y2)/(n1+n2)
-              var z = ((p1 - p2) - 0) / (Math.sqrt(po * (1 - po) * (1/n1 + 1/n2)))
-              var pValue = GetZPercent(z)
-              nucleotides.push(pValue)   
-            })
-            var newTwomer = new Object();
-            newTowmer = {
-              A: nucleotides[0],
-              C: nucleotides[1],
-              G: nucleotides[2],
-              T: nucleotides[3]
-            }
-            pValueObjects.twomers.push(newTwomer)
+    nucleotides = new Array()
 
-          }
-    //parse threemers
-    for (i = 0; i < parsed1.threemers.length; i++){
-      
-            var threemer1 = parsed1.threemers[i];
-            var n1 = 0;
-            Object.keys(threemer1).forEach(function(nucleotide){
-              n1 = n1 + threemer1[nucleotide].count;
-            })
-      
-            var threemer2 = parsed2.threemers[i];
-            var n2 = 0;
-            Object.keys(threemer2).forEach(function(nucleotide){
-              n2 = n2 + threemer2[nucleotide].count;
-            })
+    Object.keys(twomer1).forEach(function(nucleotide) {
+      var p1 = twomer1[nucleotide].proportion
+      var p2 = twomer2[nucleotide].proportion
+      var y1 = twomer1[nucleotide].count
+      var y2 = twomer2[nucleotide].count
+      var po = (y1 + y2) / (n1 + n2)
+      var z = (p1 - p2 - 0) / Math.sqrt(po * (1 - po) * (1 / n1 + 1 / n2))
+      var pValue = GetZPercent(z)
+      nucleotides.push(pValue)
+    })
+    var newTwomer = new Object()
+    newTowmer = {
+      A: nucleotides[0],
+      C: nucleotides[1],
+      G: nucleotides[2],
+      T: nucleotides[3]
+    }
+    pValueObjects.twomers.push(newTwomer)
+  }
+  //parse threemers
+  for (i = 0; i < parsed1.threemers.length; i++) {
+    var threemer1 = parsed1.threemers[i]
+    var n1 = 0
+    Object.keys(threemer1).forEach(function(nucleotide) {
+      n1 = n1 + threemer1[nucleotide].count
+    })
 
-            nucleotides = new Array();
-            
-            Object.keys(threemer1).forEach(function(nucleotide){
-              var p1 =  threemer1[nucleotide].proportion
-              var p2 = threemer2[nucleotide].proportion
-              var y1 =  threemer1[nucleotide].count
-              var y2 = threemer2[nucleotide].count
-              var po = (y1+y2)/(n1+n2)
-              var z = ((p1 - p2) - 0) / (Math.sqrt(po * (1 - po) * (1/n1 + 1/n2)))
-              var pValue = GetZPercent(z)
-              nucleotides.push(pValue)   
-            })
+    var threemer2 = parsed2.threemers[i]
+    var n2 = 0
+    Object.keys(threemer2).forEach(function(nucleotide) {
+      n2 = n2 + threemer2[nucleotide].count
+    })
 
-            var newThreemer = new Object();
-            newThreemer = {
-              A: nucleotides[0],
-              C: nucleotides[1],
-              G: nucleotides[2],
-              T: nucleotides[3]
-            }
-            pValueObjects.threemers.push(newThreemer)
-          }
+    nucleotides = new Array()
 
-    //parse fourmers
-    for (i = 0; i < parsed1.fourmers.length; i++){
-      
-            var fourmer1 = parsed1.fourmers[i];
-            var n1 = 0;
-            Object.keys(fourmer1).forEach(function(nucleotide){
-              n1 = n1 + fourmer1[nucleotide].count;
-            })
-      
-            var fourmer2 = parsed2.fourmers[i];
-            var n2 = 0;
-            Object.keys(fourmer2).forEach(function(nucleotide){
-              n2 = n2 + fourmer2[nucleotide].count;
-            })
+    Object.keys(threemer1).forEach(function(nucleotide) {
+      var p1 = threemer1[nucleotide].proportion
+      var p2 = threemer2[nucleotide].proportion
+      var y1 = threemer1[nucleotide].count
+      var y2 = threemer2[nucleotide].count
+      var po = (y1 + y2) / (n1 + n2)
+      var z = (p1 - p2 - 0) / Math.sqrt(po * (1 - po) * (1 / n1 + 1 / n2))
+      var pValue = GetZPercent(z)
+      nucleotides.push(pValue)
+    })
 
-            nucleotides = new Array();
-            
-            Object.keys(fourmer1).forEach(function(nucleotide){
-              var p1 =  fourmer1[nucleotide].proportion
-              var p2 = fourmer2[nucleotide].proportion
-              var y1 =  fourmer1[nucleotide].count
-              var y2 = fourmer2[nucleotide].count
-              var po = (y1+y2)/(n1+n2)
-              var z = ((p1 - p2) - 0) / (Math.sqrt(po * (1 - po) * (1/n1 + 1/n2)))
-              var pValue = GetZPercent(z)
-              nucleotides.push(pValue)   
-            })
+    var newThreemer = new Object()
+    newThreemer = {
+      A: nucleotides[0],
+      C: nucleotides[1],
+      G: nucleotides[2],
+      T: nucleotides[3]
+    }
+    pValueObjects.threemers.push(newThreemer)
+  }
 
-            var newFourmer = new Object();
-            newFourmer = {
-              A: nucleotides[0],
-              C: nucleotides[1],
-              G: nucleotides[2],
-              T: nucleotides[3]
-            }
-            pValueObjects.fourmers.push(newFourmer)
-          }
-          debugger
+  //parse fourmers
+  for (i = 0; i < parsed1.fourmers.length; i++) {
+    var fourmer1 = parsed1.fourmers[i]
+    var n1 = 0
+    Object.keys(fourmer1).forEach(function(nucleotide) {
+      n1 = n1 + fourmer1[nucleotide].count
+    })
+
+    var fourmer2 = parsed2.fourmers[i]
+    var n2 = 0
+    Object.keys(fourmer2).forEach(function(nucleotide) {
+      n2 = n2 + fourmer2[nucleotide].count
+    })
+
+    nucleotides = new Array()
+
+    Object.keys(fourmer1).forEach(function(nucleotide) {
+      var p1 = fourmer1[nucleotide].proportion
+      var p2 = fourmer2[nucleotide].proportion
+      var y1 = fourmer1[nucleotide].count
+      var y2 = fourmer2[nucleotide].count
+      var po = (y1 + y2) / (n1 + n2)
+      var z = (p1 - p2 - 0) / Math.sqrt(po * (1 - po) * (1 / n1 + 1 / n2))
+      var pValue = GetZPercent(z)
+      nucleotides.push(pValue)
+    })
+
+    var newFourmer = new Object()
+    newFourmer = {
+      A: nucleotides[0],
+      C: nucleotides[1],
+      G: nucleotides[2],
+      T: nucleotides[3]
+    }
+    pValueObjects.fourmers.push(newFourmer)
+  }
+  return pValueObjects
 }
 
-const GetZPercent = (z) => {
+const GetZPercent = z => {
   //z == number of standard deviations from the mean
 
   //if z is greater than 6.5 standard deviations from the mean
-  //the number of significant digits will be outside of a reasonable 
+  //the number of significant digits will be outside of a reasonable
   //range
-  if ( z < -6.5)
-    return 0.0;
-  if( z > 6.5) 
+  if (z < -6.5) return 0.0
+  if (z > 6.5)
     //return 1.0;
-    return 0.0;
+    return 0.0
 
-  var factK = 1;
-  var sum = 0;
-  var term = 1;
-  var k = 0;
-  var loopStop = Math.exp(-23);
-  while(Math.abs(term) > loopStop) 
-  {
-    term = .3989422804 * Math.pow(-1,k) * Math.pow(z,k) / (2 * k + 1) / Math.pow(2,k) * Math.pow(z,k+1) / factK;
-    sum += term;
-    k++;
-    factK *= k;
-
+  var factK = 1
+  var sum = 0
+  var term = 1
+  var k = 0
+  var loopStop = Math.exp(-23)
+  while (Math.abs(term) > loopStop) {
+    term =
+      0.3989422804 *
+      Math.pow(-1, k) *
+      Math.pow(z, k) /
+      (2 * k + 1) /
+      Math.pow(2, k) *
+      Math.pow(z, k + 1) /
+      factK
+    sum += term
+    k++
+    factK *= k
   }
-  sum += 0.5;
+  sum += 0.5
 
-  if (z < 0){
-    return sum * 2;
-  }
-  else{
-    return ((1-sum) * 2)
+  if (z < 0) {
+    return sum * 2
+  } else {
+    return (1 - sum) * 2
   }
 }
 
@@ -359,16 +369,18 @@ const awaitProcessing = (f1, f2) => {
 }
 
 const updateTables = pValueTable => {
+  console.log(pValueTable)
   for (let key in pValueTable) {
-    let tableString = "<table><tbody>"
+    let tableString = `<div>${key}</div>`
+    tableString += '<table style="margin: 16px">'
     for (let j = 0; j < pValueTable[key].length; j++) {
       tableString += "<tr>"
-      for (let k = 0; k < pValueTable[key][j]; k++) {
-        tableString += `<td>${pValueTable[key][j][k]}</td>`
+      for (let nuc in pValueTable[key][j]) {
+        tableString += `<td>${pValueTable[key][j][nuc]}</td>`
       }
       tableString += "</tr>"
     }
-    tableString += "</tbody></table>"
+    tableString += "</table>"
     const renderNode = document.getElementById(`table${key}`)
     renderNode.innerHTML = tableString
   }
@@ -377,5 +389,5 @@ const updateTables = pValueTable => {
 // Init function
 ;(function() {
   const goButton = document.getElementById("go")
-  goButton.addEventListener("click", beginProcessing) 
+  goButton.addEventListener("click", beginProcessing)
 })()
